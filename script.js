d4f45572a17eb2e7321d13fd46ac6dd96bb8205b
Kumar Rajeev <117367016+Kumar-Raajeev@users.noreply.github.com>
2023-07-16 03:29:27 +0530

    Added the 2nd navbar and the ads bar in page

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var buttons = document.getElementsByClassName("button");
+var button = document.getElementsByClassName("button");
 
 // Add event listener to each button
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function() {
+for (var i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", function() {
     // Remove active class from all buttons
-    for (var j = 0; j < buttons.length; j++) {
-      buttons[j].classList.remove("active");
+    for (var j = 0; j < button.length; j++) {
+      button[j].classList.remove("active");
     }
 
     // Add active class to the clicked button
